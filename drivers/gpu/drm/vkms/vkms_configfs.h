5ea9219660d06d81f4a13f1084be9a5f9d53c3f8
Louis Chauvet <louis.chauvet@bootlin.com>
2024-08-07 09:24:26 +0200

    drm/vkms: Introduce configfs for crtc and encoder
    To allows the userspace to test many hardware configuration, introduce a
    new interface to configure CRTCs and encoders.
    
    The CRTCs and encoders are created in their own directory. To link the
    CRTC, symlinks are used in the `possible_crtcs` folders.
    
    The current interface is:
    /config/vkms
    	DEVICE_1
    	┣━ enable
    	┣━ planes
    	┃  ┗━ PLANE_1
    	┃     ┣━ type
    	┃     ┣━ supported_rotations
    	┃     ┣━ supported_color_encoding
    	┃     ┣━ supported_color_ranges
    	┃     ┣━ default_rotation
    	┃     ┣━ default_color_encoding
    	┃     ┣━ default_color_range
    	┃     ┗━ possible_crtcs
    	┃        ┗━ >> /config/vkms/DEVICE_1/crtcs/CRTC_1
    	┣━ encoders
    	┃  ┗━ ENCODER_1
    	┃     ┗━ possible_crtcs
    	┃        ┗━ >> /config/vkms/DEVICE_1/crtcs/CRTC_1
    	┣━ crtcs
    	┃  ┗━ CRTC_1
    	DEVICE_2
    	┗━ ditto
    
    Signed-off-by: Louis Chauvet <louis.chauvet@bootlin.com>

--- drivers/gpu/drm/vkms/vkms_configfs.h
+++ drivers/gpu/drm/vkms/vkms_configfs.h
@@ -20,20 +20,37 @@ struct vkms_configfs_device {
 	struct config_group group;
 
 	struct config_group plane_group;
+	struct config_group crtc_group;
+	struct config_group encoder_group;
 
 	struct mutex lock;
 	bool enabled;
 
 	struct vkms_config *vkms_config;
-	struct vkms_config_crtc *vkms_config_crtc;
-	struct vkms_config_encoder *vkms_config_encoder;
 };
 
 struct vkms_configfs_plane {
 	struct config_group group;
+	struct config_group possible_crtc_group;
 
 	struct vkms_configfs_device *vkms_configfs_device;
 	struct vkms_config_plane *vkms_config_plane;
+};
+
+struct vkms_configfs_crtc {
+	struct config_group group;
+
+	struct vkms_configfs_device *vkms_configfs_device;
+	struct vkms_config_crtc *vkms_config_crtc;
+};
+
+struct vkms_configfs_encoder {
+	/* must be first because it is a krefcounted stuff */
+	struct config_group group;
+
+	struct config_group possible_crtc_group;
+	struct vkms_configfs_device *vkms_configfs_device;
+	struct vkms_config_encoder *vkms_config_encoder;
 };
 
 #define config_item_to_vkms_configfs_device(item) \
@@ -42,11 +59,44 @@ struct vkms_configfs_plane {
 #define planes_item_to_vkms_configfs_device(item) \
 	config_item_to_vkms_configfs_device((item)->ci_parent)
 
+#define encoders_item_to_vkms_configfs_device(item) \
+	config_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define crtc_item_to_vkms_configfs_crtc(item) \
+	container_of(to_config_group((item)), struct vkms_configfs_crtc, group)
+
+#define encoder_item_to_vkms_configfs_encoder(item) \
+	container_of(to_config_group((item)), struct vkms_configfs_encoder, group)
+
 #define plane_item_to_vkms_configfs_device(item) \
 	planes_item_to_vkms_configfs_device((item)->ci_parent)
 
 #define plane_item_to_vkms_configfs_plane(item) \
 	container_of(to_config_group((item)), struct vkms_configfs_plane, group)
+
+#define plane_possible_crtc_src_item_to_vkms_configfs_device(item) \
+	plane_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define plane_possible_crtc_src_item_to_vkms_configfs_plane(item) \
+	plane_item_to_vkms_configfs_plane((item)->ci_parent)
+
+#define crtc_item_to_vkms_configfs_device(item) \
+	config_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define crtc_child_item_to_vkms_configfs_device(item) \
+	crtc_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define encoder_item_to_vkms_configfs_device(item) \
+	config_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define encoder_child_item_to_vkms_configfs_device(item) \
+	encoder_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define encoder_possible_crtc_src_item_to_vkms_configfs_device(item) \
+	encoder_child_item_to_vkms_configfs_device((item)->ci_parent)
+
+#define encoder_possible_crtc_src_item_to_vkms_configfs_encoder(item) \
+	encoder_item_to_vkms_configfs_encoder((item)->ci_parent)
 
 /* ConfigFS Support */
 int vkms_init_configfs(void);
