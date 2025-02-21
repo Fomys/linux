385a8b562423bd0960bfb26a678f737f39848f6d
Louis Chauvet <louis.chauvet@bootlin.com>
2025-02-21 03:25:10 +0100

    drm/vkms: Add transfer handler
    Signed-off-by: Louis Chauvet <louis.chauvet@bootlin.com>

--- drivers/gpu/drm/vkms/vkms_mst.c
+++ drivers/gpu/drm/vkms/vkms_mst.c
@@ -16,14 +16,124 @@ static void vkms_mst_emulator_init_memory(struct vkms_dpcd_memory * dpcd_memory)
 	dpcd_memory->DPCD_REV = DP_DPCD_REV_14;
 }
 
-void vkms_mst_emulator_init(struct vkms_mst_emulator *emulator,	const char *name)
+void vkms_mst_emulator_init(struct vkms_mst_emulator *emulator,
+			    const struct vkms_mst_transfer_helpers *transfer_helpers,
+			    const char *name)
 {
 	vkms_mst_emulator_init_memory(&emulator->dpcd_memory);
-	memset(&emulator->dpcd_memory, 0, sizeof(emulator->dpcd_memory));
+
+	emulator->transfer_helpers = transfer_helpers;
 	emulator->name = kstrdup_const(name, GFP_KERNEL);
 }
 
 void vkms_mst_emulator_destroy(struct vkms_mst_emulator *emulator)
 {
 	kfree_const(emulator->name);
+}
+
+ssize_t vkms_mst_emulator_transfer_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg)
+{
+	if (emulator->transfer_helpers) {
+		switch (msg->request & ~DP_AUX_I2C_MOT) {
+		case DP_AUX_NATIVE_READ:
+			if (emulator->transfer_helpers->transfer_read)
+				return emulator->transfer_helpers->transfer_read(emulator, port_id, msg);
+			break;
+		case DP_AUX_NATIVE_WRITE:
+			if (emulator->transfer_helpers->transfer_write)
+				return emulator->transfer_helpers->transfer_write(emulator, port_id, msg);
+			break;
+		case DP_AUX_I2C_WRITE:
+			if (emulator->transfer_helpers->transfer_i2c_write)
+				return emulator->transfer_helpers->transfer_i2c_write(emulator, port_id, msg);
+			break;
+		case DP_AUX_I2C_READ:
+			if (emulator->transfer_helpers->transfer_i2c_read)
+				return emulator->transfer_helpers->transfer_i2c_read(emulator, port_id, msg);
+			break;
+		default:
+			break;
+		}
+	}
+
+	return -EPROTO;
+}
+
+ssize_t vkms_mst_emulator_transfer_read_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg)
+{
+	if (msg->request != DP_AUX_NATIVE_READ) {
+		pr_err("Wrong request type, the caller must only call this with DP_AUX_NATIVE_READ requests.\n");
+		return -EPROTO;
+	}
+	u8 *buffer = msg->buffer;
+	msg->reply = DP_AUX_NATIVE_REPLY_ACK;
+
+	int i = 0;
+	while (i < msg->size) {
+		int curr_offset = msg->address + i;
+		switch (curr_offset) {
+		case DP_DPCD_REV:
+			*buffer = emulator->dpcd_memory.DPCD_REV;
+			break;
+		default:
+			*buffer = 0;
+			msg->reply = DP_AUX_NATIVE_REPLY_NACK;
+			pr_err("Requested reading unsupported register %d\n", curr_offset);
+			return -EPROTO;
+		}
+
+		buffer++;
+		i++;
+	}
+
+	return i;
+}
+
+ssize_t vkms_mst_emulator_transfer_write_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg)
+{
+	if (msg->request != DP_AUX_NATIVE_WRITE) {
+		pr_err("Wrong request type, the caller must only call this with DP_AUX_NATIVE_WRITE requests.\n");
+		return 0;
+	}
+	u8 *buffer = msg->buffer;
+	msg->reply = DP_AUX_NATIVE_REPLY_ACK;
+
+	int i = 0;
+	while (i < msg->size) {
+		int curr_offset = msg->address + i;
+
+		switch (curr_offset) {
+		default:
+			*buffer = 0;
+			msg->reply = DP_AUX_NATIVE_REPLY_NACK;
+			pr_err("Requested write unsupported register %d\n", curr_offset);
+			return -EPROTO;
+		}
+
+		buffer++;
+		i++;
+	}
+	return i;
+}
+
+ssize_t vkms_mst_emulator_transfer_i2c_write_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg)
+{
+	if ((msg->request & ~DP_AUX_I2C_MOT) != DP_AUX_I2C_WRITE) {
+		pr_err("Wrong request type, the caller must only call this with DP_AUX_I2C_WRITE requests.\n");
+		return 0;
+	}
+	msg->reply = DP_AUX_I2C_REPLY_NACK;
+
+	return -EPROTO;
+}
+
+ssize_t vkms_mst_emulator_transfer_i2c_read_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg)
+{
+	if ((msg->request & ~DP_AUX_I2C_MOT) != DP_AUX_I2C_READ) {
+		pr_err("Wrong request type, the caller must only call this with DP_AUX_I2C_READ requests.\n");
+		return 0;
+	}
+	msg->reply = DP_AUX_I2C_REPLY_NACK;
+
+	return -EPROTO;
 }
--- drivers/gpu/drm/vkms/vkms_mst.h
+++ drivers/gpu/drm/vkms/vkms_mst.h
@@ -2,6 +2,7 @@
 #define _VKMS_MST_H_
 
 #include <linux/types.h>
+#include <drm/display/drm_dp_helper.h>
 
 /**
  * struct vkms_dpcd_memory - Representation of the DPCD memory of a device
@@ -11,15 +12,62 @@ struct vkms_dpcd_memory {
 	u8 DPCD_REV;
 };
 
+struct vkms_mst_emulator;
+
+/**
+ * struct vkms_mst_transfer_helpers - Helpers to emulate a memory transfer over
+ * dp-aux channel.
+ *
+ * You can find default implementation for those functions at vkms_mst.h.
+ *
+ * @transfer: Called when a transfer is received on @port_id.
+ *	      Default implementation: vkms_mst_emulator_transfer_default
+ * @transfer_read: If using vkms_mst_emulator_transfer_default,
+ * 		   this is called for each native read request received
+ *		   on @port_id.
+ *		   Default implementation: vkms_mst_emulator_transfer_write_default
+ * @transfer_write: If using vkms_mst_emulator_transfer_default,
+ * 		    this is called for each native write request received
+ *		    on @port_id.
+ *		    Default implementation: vkms_mst_emulator_transfer_read_default
+ * @transfer_i2c_write: If using vkms_mst_emulator_transfer_default,
+ * 		        this is called for each i2c write request
+ *		        received on @port_id.
+ *		        Default implementation: vkms_mst_emulator_transfer_i2c_write_default
+ * @transfer_i2c_read: If using vkms_mst_emulator_transfer_default,
+ * 		       this is called for each i2c read request
+ *		       received on @port_id.
+ *		       Default implementation: vkms_mst_emulator_transfer_i2c_read_default
+ */
+struct vkms_mst_transfer_helpers {
+	ssize_t (*transfer)(struct vkms_mst_emulator *emulator,
+				   u8 port_id, struct drm_dp_aux_msg *msg);
+	ssize_t (*transfer_read)(struct vkms_mst_emulator *emulator,
+					u8 port_id, struct drm_dp_aux_msg *msg);
+	ssize_t (*transfer_write)(struct vkms_mst_emulator *emulator,
+					 u8 port_id, struct drm_dp_aux_msg *msg);
+	ssize_t (*transfer_i2c_write)(struct vkms_mst_emulator *emulator,
+					     u8 port_id,
+					     struct drm_dp_aux_msg *msg);
+	ssize_t (*transfer_i2c_read)(struct vkms_mst_emulator *emulator,
+					    u8 port_id,
+					    struct drm_dp_aux_msg *msg);
+};
+
 /**
  * struct vkms_mst_emulator - Base structure for all MST device emulators.
  *
  * @dpcd_memory: Representation of the internal DPCD memory. This is a private
  *               field that should not be accessed outside the device itself.
+ *               This memory is updated and read by the default implementation
+ *               vkms_mst_emulator_transfer_read/write_default
+ * @transfer_helpers: helpers called when a dp-aux transfer is requested
  * @name: Name of the device. Mainly used for logging purpose.
  */
 struct vkms_mst_emulator {
 	struct vkms_dpcd_memory dpcd_memory;
+
+	const struct vkms_mst_transfer_helpers *transfer_helpers;
 
 	const char *name;
 };
@@ -27,14 +75,75 @@ struct vkms_mst_emulator {
 /**
  * vkms_mst_emulator_init - Initialize an MST emulator device
  * @emulator: Structure to initialize
+ * @transfer_helpers: Helpers used to emulate dp-aux transfers. Can be NULL.
  * @name: Name of the device. Used mainly for logging purpose.
  */
-void vkms_mst_emulator_init(struct vkms_mst_emulator *emulator,	const char *name);
+void vkms_mst_emulator_init(struct vkms_mst_emulator *emulator,
+			    const struct vkms_mst_transfer_helpers *transfer_helpers,
+			    const char *name);
 
 /**
  * vkms_mst_emulator_destroy: Destroy all resources allocated for an emulator
  * @emulator: Device to destroy
  */
 void vkms_mst_emulator_destroy(struct vkms_mst_emulator *emulator);
+
+/**
+ * vkms_mst_emulator_transfer_default - Default implementation to handle a
+ * dp-aux transfer.
+ *
+ * This implementation automatically call the correct callback in
+ * @emulator->transfer_helper.
+ */
+ssize_t vkms_mst_emulator_transfer_default(struct vkms_mst_emulator *emulator,
+					   u8 port_id, struct drm_dp_aux_msg *msg);
+
+/**
+ * vkms_mst_emulator_transfer_read_default - Default implementation to hand
+ * native read on dp-aux
+ * @emulator: Destination for the transfer
+ * @port_id: Port which received the transfer
+ * @msg: Requested transfer
+ *
+ * This implementation simply read the requested values from the correct
+ * register.
+ * Not all the registers are readable.
+ */
+ssize_t vkms_mst_emulator_transfer_read_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg);
+
+/**
+ * vkms_mst_emulator_transfer_write_default - Default implementation to hand
+ * native write on dp-aux
+ * @emulator: Destination for the transfer
+ * @port_id: Port which received the transfer
+ * @msg: Requested transfer
+ *
+ * This implementation simply write the requested values in the correct
+ * register.
+ * Not all the registers are writable.
+ */
+ssize_t vkms_mst_emulator_transfer_write_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg);
+
+/**
+ * vkms_mst_emulator_transfer_i2c_write_default - Default implementation to
+ * handle i2c write over dp-aux
+ * @emulator: Destination for the transfer
+ * @port_id: Port which received the transfer
+ * @msg: Requested transfer
+ *
+ * This implementation simply reply with a NACK.
+ */
+ssize_t vkms_mst_emulator_transfer_i2c_write_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg);
+
+/**
+ * vkms_mst_emulator_transfer_i2c_read_default - Default implementation to
+ * handle i2c read over dp-aux
+ * @emulator: Destination for the transfer
+ * @port_id: Port which received the transfer
+ * @msg: Requested transfer
+ *
+ * This implementation simply reply with a NACK.
+ */
+ssize_t vkms_mst_emulator_transfer_i2c_read_default(struct vkms_mst_emulator *emulator, u8 port_id, struct drm_dp_aux_msg *msg);
 
 #endif
